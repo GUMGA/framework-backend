9eb212957943202e51b7559b78509e9631c7fe60
Everton Emilio Tavares <everton.tavares@db1.com.br>
2014-06-06 17:29:52 -0300

    Tratamento das exceções padrão

--- gumga-presentation/src/main/java/gumga/framework/presentation/GlobalExceptionHandler.java
+++ gumga-presentation/src/main/java/gumga/framework/presentation/GlobalExceptionHandler.java
@@ -1,6 +1,10 @@
 package gumga.framework.presentation;
 
-
+import gumga.framework.core.exception.BadRequestException;
+import gumga.framework.core.exception.ConflictException;
+import gumga.framework.core.exception.ForbiddenException;
+import gumga.framework.core.exception.NotFoundException;
+import gumga.framework.core.exception.UnauthorizedException;
 import gumga.framework.presentation.validation.ErrorResource;
 import gumga.framework.presentation.validation.FieldErrorResource;
 import gumga.framework.validation.exception.InvalidEntityException;
@@ -29,59 +33,106 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-	
+
 	private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
-	
+
 	@ExceptionHandler(InvalidEntityException.class)
 	public ResponseEntity<Object> handleCustomException(InvalidEntityException ex, WebRequest request) {
-		
+
 		List<FieldErrorResource> fieldErrorResources = new ArrayList<>();
 
-        List<FieldError> fieldErrors = ex.getErrors().getFieldErrors();
-        
-        for (FieldError fieldError : fieldErrors) {
-            FieldErrorResource fieldErrorResource = new FieldErrorResource();
-            fieldErrorResource.setResource(fieldError.getObjectName());
-            fieldErrorResource.setField(fieldError.getField());
-            fieldErrorResource.setCode(fieldError.getCode());
-            fieldErrorResource.setMessage(fieldError.getDefaultMessage());
-            fieldErrorResources.add(fieldErrorResource);
-        }
+		List<FieldError> fieldErrors = ex.getErrors().getFieldErrors();
 
-        ErrorResource error = new ErrorResource("InvalidRequest", ex.getMessage());
-        error.setFieldErrors(fieldErrorResources);
+		for (FieldError fieldError : fieldErrors) {
+			FieldErrorResource fieldErrorResource = new FieldErrorResource();
+			fieldErrorResource.setResource(fieldError.getObjectName());
+			fieldErrorResource.setField(fieldError.getField());
+			fieldErrorResource.setCode(fieldError.getCode());
+			fieldErrorResource.setMessage(fieldError.getDefaultMessage());
+			fieldErrorResources.add(fieldErrorResource);
+		}
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
+		ErrorResource error = new ErrorResource("InvalidRequest", ex.getMessage());
+		error.setFieldErrors(fieldErrorResources);
 
-        logger.info("InvalidEntity", ex);
-        return handleExceptionInternal(ex, error, headers, HttpStatus.UNPROCESSABLE_ENTITY, request);
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		logger.info("InvalidEntity", ex);
+		return handleExceptionInternal(ex, error, headers, HttpStatus.UNPROCESSABLE_ENTITY, request);
 	}
-	
-	@ExceptionHandler(EntityNotFoundException.class)
-	@ResponseStatus(HttpStatus.NOT_FOUND)
-	public @ResponseBody ErrorResource notFound(HttpServletRequest req, Exception ex) {
-		logger.warn("ResourceNotFound", ex);
-		return new ErrorResource("EntityNotFound", "Entity not found", ex.getMessage());
-	}
-	
+
 	@ExceptionHandler(IllegalArgumentException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public @ResponseBody ErrorResource badRequest(HttpServletRequest req, Exception ex) {
-		logger.warn("BadRequest", ex);
+	public @ResponseBody
+	ErrorResource illegalArgument(HttpServletRequest req, Exception ex) {
+		logger.warn("IllegalArgument", ex);
 		return new ErrorResource("IllegalArgument", "Invalid request", ex.getMessage());
 	}
-	
-	@ExceptionHandler({ConstraintViolationException.class, DataIntegrityViolationException.class})
+
+	@ExceptionHandler({ ConstraintViolationException.class, DataIntegrityViolationException.class })
 	@ResponseStatus(HttpStatus.CONFLICT)
-	public @ResponseBody ErrorResource constraintViolation(HttpServletRequest req, Exception ex) {
+	public @ResponseBody
+	ErrorResource constraintViolation(HttpServletRequest req, Exception ex) {
 		logger.warn("Error on operation", ex);
 		return new ErrorResource("ConstraintViolation", "Error on operation", ex.getMessage());
 	}
-	
-	@ExceptionHandler(Exception.class)
+
+	/*
+	 * Tratamento das exceções padrão
+	 */
+
+	@ExceptionHandler({ EntityNotFoundException.class, NotFoundException.class })
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public @ResponseBody
+	ErrorResource notFound(HttpServletRequest req, Exception ex) {
+		logger.warn("ResourceNotFound", ex);
+		return new ErrorResource("ResourceNotFound", "Entity not found", ex.getMessage());
+	}
+
+	@ExceptionHandler(BadRequestException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public @ResponseBody
+	ErrorResource badRequest(HttpServletRequest req, Exception ex) {
+		logger.warn("BadRequest", ex);
+		return new ErrorResource("BadRequest", "Invalid request", ex.getMessage());
+	}
+
+	@ExceptionHandler({ ConflictException.class })
 	@ResponseStatus(HttpStatus.CONFLICT)
-	public @ResponseBody ErrorResource exception(HttpServletRequest req, Exception ex) {
+	public @ResponseBody
+	ErrorResource conflict(HttpServletRequest req, Exception ex) {
+		logger.warn("Conflict", ex);
+		return new ErrorResource("Conflict", "Error on operation", ex.getMessage());
+	}
+
+	@ExceptionHandler({ ForbiddenException.class })
+	@ResponseStatus(HttpStatus.FORBIDDEN)
+	public @ResponseBody
+	ErrorResource forbidden(HttpServletRequest req, Exception ex) {
+		logger.warn("Forbidden", ex);
+		return new ErrorResource("Forbidden", "Error on operation", ex.getMessage());
+	}
+
+	@ExceptionHandler({ UnauthorizedException.class })
+	@ResponseStatus(HttpStatus.UNAUTHORIZED)
+	public @ResponseBody
+	ErrorResource unauthorized(HttpServletRequest req, Exception ex) {
+		logger.warn("Unauthorized", ex);
+		return new ErrorResource("Unauthorized", "Error on operation", ex.getMessage());
+	}
+
+	/**
+	 * Todo erro não tratado irá lançar um "INTERNAL SERVER ERROR", que corresponde ao status 500
+	 * 
+	 * @param req
+	 * @param ex
+	 * @return
+	 */
+	@ExceptionHandler(Exception.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public @ResponseBody
+	ErrorResource exception(HttpServletRequest req, Exception ex) {
 		logger.error("Error on operation", ex);
 		return new ErrorResource(ex.getClass().getSimpleName(), "Error on operation", ex.getMessage());
 	}
