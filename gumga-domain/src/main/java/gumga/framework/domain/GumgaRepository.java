e9a58b1e379bab0abf58e3232a1d31a658c4a1e6
Gabriel Such <gabrielsuch@gmail.com>
2014-06-18 15:10:10 -0300

    remocao de metodos duplicados no repository

--- gumga-domain/src/main/java/gumga/framework/domain/GumgaRepository.java
+++ gumga-domain/src/main/java/gumga/framework/domain/GumgaRepository.java
@@ -1,22 +1,12 @@
 package gumga.framework.domain;
 
-import static org.hibernate.criterion.Order.asc;
-import static org.hibernate.criterion.Order.desc;
-import static org.hibernate.criterion.Projections.rowCount;
-import static org.hibernate.criterion.Restrictions.or;
 import gumga.framework.core.GumgaIdable;
-import gumga.framework.core.QueryObject;
-import gumga.framework.core.SearchResult;
 import gumga.framework.core.utils.ReflectionUtils;
-
-import java.util.List;
 
 import javax.persistence.EntityNotFoundException;
 
-import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Criterion;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -64,79 +54,10 @@ public class GumgaRepository<T extends GumgaIdable> {
 		return clazz;
 	}
 	
-	@Transactional(readOnly = true)
-	public SearchResult<T> pesquisa(QueryObject query) {
-		Long count = count(query);
-		List<T> data = getOrdered(query);
-		
-		return new SearchResult<T>(query, count, data);
-	}
-	
-	private List<T> getOrdered(QueryObject query) {
-		Pesquisa<T> pesquisa = getPesquisa(query);
-		String sortField = query.getSortField();
-		String sortType = query.getSortDir();
-		
-		if (!sortField.isEmpty()) {
-			createAliasIfNecessary(pesquisa, sortField);
-			pesquisa.addOrder(sortType.equals("asc") ? asc(sortField).ignoreCase() : desc(sortField).ignoreCase());
-		}
-		
-		return pesquisa.setFirstResult(query.getStart()).setMaxResults(query.getPageSize()).list();
-	}
-
-	private Long count(QueryObject query) {
-		Object uniqueResult = getPesquisa(query).setProjection(rowCount()).uniqueResult();
-		return uniqueResult == null ? 0L : ((Number) uniqueResult).longValue();
-	}
-	
-	protected Pesquisa<T> getPesquisa(QueryObject query) {
-		if (query.getSearchFields() != null && query.getSearchFields().length == 0)
-			throw new IllegalArgumentException();
-		
-		Criterion[] fieldsCriterions = new HibernateQueryObject(query).getCriterions(clazz());
-		Pesquisa<T> pesquisa = pesquisa().add(or(fieldsCriterions));
-		
-		if (query.getSearchFields() != null) 
-			for (String field : query.getSearchFields()) 
-				createAliasIfNecessary(pesquisa, field);
-		
-		return pesquisa;
-	}
-	
-	private void createAliasIfNecessary(Pesquisa<T> pesquisa, String field) {
-		String[] chain = field.split("\\.");
-		
-		if (chain.length <= 1) return;
-		if (pesquisa.getAliases().contains(chain[0])) return;
-		
-		pesquisa.createAlias(chain[0], chain[0]);
-		pesquisa.addAlias(chain[0]);
-	}
-	
-	@Transactional(readOnly = true)
-	public Pesquisa<T> pesquisa() {
-		return Pesquisa.createCriteria(session(), clazz());
-	}
-	
-	public Query createQuery(String query) {
-		return session().createQuery(query);
-	}
-	
-	public Query createSQLQuery(String query) {
-		return session().createSQLQuery(query);
-	}
-	
 	private Session session() {
 		return sessionFactory.getCurrentSession();
 	}
-	
-	@Transactional(readOnly = true)
-	public Long count() {
-		Number count = (Number) pesquisa().setProjection(rowCount()).uniqueResult();
-		return count.longValue();
-	}
-	
+		
 	public void setClazz(Class<T> clazz) {
 		this.clazz = clazz;
 	}
