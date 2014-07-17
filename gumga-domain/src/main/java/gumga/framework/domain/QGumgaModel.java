20a2e61f5173517995089be0f9cdf4d85af3a556
Gabriel Such <gabrielsuch@gmail.com>
2014-07-17 16:53:57 -0300

    suporte a querydsl

---
+++ gumga-domain/src/main/java/gumga/framework/domain/QGumgaModel.java
@@ -0,0 +1,40 @@
+package gumga.framework.domain;
+
+import static com.mysema.query.types.PathMetadataFactory.forVariable;
+
+import javax.annotation.Generated;
+
+import com.mysema.query.types.Path;
+import com.mysema.query.types.PathMetadata;
+import com.mysema.query.types.path.EntityPathBase;
+import com.mysema.query.types.path.SimplePath;
+
+
+/**
+ * QGumgaModel is a Querydsl query type for GumgaModel
+ */
+@Generated("com.mysema.query.codegen.SupertypeSerializer")
+public class QGumgaModel extends EntityPathBase<GumgaModel<? extends java.io.Serializable>> {
+
+    private static final long serialVersionUID = 1949626837L;
+
+    public static final QGumgaModel gumgaModel = new QGumgaModel("gumgaModel");
+
+    public final SimplePath<java.io.Serializable> id = createSimple("id", java.io.Serializable.class);
+
+    @SuppressWarnings("all")
+    public QGumgaModel(String variable) {
+        super((Class)GumgaModel.class, forVariable(variable));
+    }
+
+    @SuppressWarnings("all")
+    public QGumgaModel(Path<? extends GumgaModel> path) {
+        super((Class)path.getType(), path.getMetadata());
+    }
+
+    @SuppressWarnings("all")
+    public QGumgaModel(PathMetadata<?> metadata) {
+        super((Class)GumgaModel.class, metadata);
+    }
+
+}
