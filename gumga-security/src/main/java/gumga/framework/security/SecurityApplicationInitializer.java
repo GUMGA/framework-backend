537635760f8938634d6a1d27318e3190f0fbeda3
Gabriel Such <gabrielsuch@gmail.com>
2014-08-01 16:58:05 -0300

    minor

--- gumga-security/src/main/java/gumga/framework/security/SecurityApplicationInitializer.java
+++ gumga-security/src/main/java/gumga/framework/security/SecurityApplicationInitializer.java
@@ -1,14 +1,21 @@
 package gumga.framework.security;
 
+import javax.servlet.ServletContext;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
 
 public class SecurityApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-	
+
 	@Override
 	protected String getDispatcherWebApplicationContextSuffix() {
 		return AbstractDispatcherServletInitializer.DEFAULT_SERVLET_NAME;
+	}
+
+	@Override
+	protected void afterSpringSecurityFilterChain(ServletContext servletContext) {
+		insertFilters(servletContext, new HiddenHttpMethodFilter());
 	}
 
 	@Override
