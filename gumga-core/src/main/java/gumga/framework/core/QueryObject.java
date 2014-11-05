72ea0d5d0c888579fde83cff290bb20f492f9281
munif <munifgebara@gmail.com>
2014-11-05 16:45:21 -0200

    QueryObject com o atributo AQ

--- gumga-core/src/main/java/gumga/framework/core/QueryObject.java
+++ gumga-core/src/main/java/gumga/framework/core/QueryObject.java
@@ -1,75 +1,100 @@
 package gumga.framework.core;
 
+import java.util.Arrays;
 
 public class QueryObject {
 
-	private String q = "";
-	private int start = 0;
-	private int pageSize = 10;
-	private String sortField = "";
-	private String sortDir = "asc";
-	private String[] searchFields;
+    public static final String SIMPLE = "SIMPLE";
 
-	public String getQ() {
-		return q;
-	}
+    private String aq = SIMPLE;
+    private String q = "";
+    private int start = 0;
+    private int pageSize = 10;
+    private String sortField = "";
+    private String sortDir = "asc";
+    private String[] searchFields;
 
-	public void setQ(String q) {
-		this.q = q;
-	}
+    public String getQ() {
+        return q;
+    }
 
-	public int getStart() {
-		return start;
-	}
+    public void setQ(String q) {
+        this.q = q;
+    }
 
-	public void setStart(int start) {
-		this.start = start;
-	}
+    public int getStart() {
+        return start;
+    }
 
-	public int getPageSize() {
-		return pageSize;
-	}
+    public void setStart(int start) {
+        this.start = start;
+    }
 
-	public void setPageSize(int pageSize) {
-		this.pageSize = pageSize;
-	}
+    public int getPageSize() {
+        return pageSize;
+    }
 
-	public String getSortField() {
-		return sortField;
-	}
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
 
-	public void setSortField(String sortField) {
-		this.sortField = sortField;
-	}
+    public String getSortField() {
+        return sortField;
+    }
 
-	public String getSortDir() {
-		return sortDir;
-	}
+    public void setSortField(String sortField) {
+        this.sortField = sortField;
+    }
 
-	public void setSortDir(String sortDir) {
-		this.sortDir = sortDir;
-	}
+    public String getSortDir() {
+        return sortDir;
+    }
 
-	public String[] getSearchFields() {
-		return searchFields;
-	}
+    public void setSortDir(String sortDir) {
+        if (Arrays.asList(new String[]{"asc", "desc"}).contains(sortDir)) {
+            this.sortDir = sortDir;
+        }
+    }
 
-	public void setSearchFields(String searchFields) {
-		this.searchFields = searchFields.split(",");
-	}
+    public String[] getSearchFields() {
+        return searchFields;
+    }
 
-	public void setSearchFields(String... fields) {
-		this.searchFields = fields;
-	}
-	
-	public int getCurrentPage() {
-		if (start == 0) return 1;
-		return start / pageSize + 1;
-	}
-	
-	public boolean isValid() {
-		return searchFields != null && searchFields.length > 0
-				&& q != null && !q.isEmpty();
-	}
+    public void setSearchFields(String searchFields) {
+        this.searchFields = searchFields.split(",");
+    }
+
+    public void setSearchFields(String... fields) {
+        this.searchFields = fields;
+    }
+
+    public int getCurrentPage() {
+        if (start == 0) {
+            return 1;
+        }
+        return start / pageSize + 1;
+    }
+
+    public boolean isValid() {
+        return searchFields != null && searchFields.length > 0
+                && q != null && !q.isEmpty();
+    }
+
+    public String getAq() {
+        return aq;
+    }
+
+    public void setAq(String aq) {
+        this.aq = aq;
+    }
+
+    public boolean isAdvanced() {
+        return !SIMPLE.equals(aq);
+    }
+
+    @Override
+    public String toString() {
+        return "QueryObject{" + "aq=" + aq + ", q=" + q + ", start=" + start + ", pageSize=" + pageSize + ", sortField=" + sortField + ", sortDir=" + sortDir + ", searchFields=" + searchFields + '}';
+    }
 
 }
