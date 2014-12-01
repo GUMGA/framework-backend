d207bb5cf8a4370c25e55bb3b7327719736301f0
munif <munifgebara@gmail.com>
2014-12-01 18:21:59 -0200

    Permite hql nos repositórios

--- gumga-application/src/main/java/gumga/framework/application/GumgaGenericRepository.java
+++ gumga-application/src/main/java/gumga/framework/application/GumgaGenericRepository.java
@@ -12,6 +12,7 @@ import gumga.framework.domain.repository.GumgaCrudRepository;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityNotFoundException;
@@ -25,98 +26,98 @@ import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
 public class GumgaGenericRepository<T, ID extends Serializable> extends SimpleJpaRepository<T, ID> implements GumgaCrudRepository<T, ID> {
-
+    
     private final JpaEntityInformation<T, ID> entityInformation;
     private final EntityManager entityManager;
-
+    
     public GumgaGenericRepository(JpaEntityInformation<T, ID> entityInformation, EntityManager entityManager) {
         super(entityInformation, entityManager);
         this.entityManager = entityManager;
         this.entityInformation = entityInformation;
     }
-
+    
     @Override
     public SearchResult<T> pesquisa(QueryObject query) {
-
+        
         if (query.isAdvanced()) {
             return advancedSearch(query);
         }
-
+        
         Long count = count(query);
         List<T> data = getOrdered(query);
-
+        
         return new SearchResult<T>(query, count, data);
     }
-
+    
     private List<T> getOrdered(QueryObject query) {
         Pesquisa<T> pesquisa = getPesquisa(query);
         String sortField = query.getSortField();
         String sortType = query.getSortDir();
-
+        
         if (!sortField.isEmpty()) {
             createAliasIfNecessary(pesquisa, sortField);
             pesquisa.addOrder(sortType.equals("asc") ? asc(sortField).ignoreCase() : desc(sortField).ignoreCase());
         }
-
+        
         return pesquisa.setFirstResult(query.getStart()).setMaxResults(query.getPageSize()).list();
     }
-
+    
     private Long count(QueryObject query) {
         Object uniqueResult = getPesquisa(query).setProjection(rowCount()).uniqueResult();
         return uniqueResult == null ? 0L : ((Number) uniqueResult).longValue();
     }
-
+    
     private Pesquisa<T> getPesquisa(QueryObject query) {
         if (query.getSearchFields() != null && query.getSearchFields().length == 0) {
             throw new IllegalArgumentException("Para realizar a pesquisa deve se informar pelo menos um campo a ser pesquisado.");
         }
-
+        
         Criterion[] fieldsCriterions = new HibernateQueryObject(query).getCriterions(entityInformation.getJavaType());
         Pesquisa<T> pesquisa = pesquisa().add(or(fieldsCriterions));
-
+        
         if (query.getSearchFields() != null) {
             for (String field : query.getSearchFields()) {
                 createAliasIfNecessary(pesquisa, field);
             }
         }
-
+        
         return pesquisa;
     }
-
+    
     private void createAliasIfNecessary(Pesquisa<T> pesquisa, String field) {
         String[] chain = field.split("\\.");
-
+        
         if (chain.length <= 1) {
             return;
         }
         if (pesquisa.getAliases().contains(chain[0])) {
             return;
         }
-
+        
         pesquisa.createAlias(chain[0], chain[0]);
         pesquisa.addAlias(chain[0]);
     }
-
+    
     @Override
     public Pesquisa<T> pesquisa() {
         return Pesquisa.createCriteria(session(), entityInformation.getJavaType());
     }
-
+    
     @Override
     public T findOne(ID id) {
         T resource = super.findOne(id);
-
+        
         if (resource == null) {
             throw new EntityNotFoundException("cannot find " + entityInformation.getJavaType() + " with id: " + id);
         }
-
+        
         return resource;
     }
-
+    
     private Session session() {
         return entityManager.unwrap(Session.class);
     }
-
+    
     private SearchResult<T> advancedSearch(QueryObject query) {
         String modelo = "from %s obj WHERE %s";
         String hqlConsulta = "";
@@ -136,5 +137,17 @@ public class GumgaGenericRepository<T, ID extends Serializable> extends SimpleJp
         List resultList = qConsulta.getResultList();
         return new SearchResult<T>(query, total, resultList);
     }
-
+    
+    public SearchResult<T> hqlSearch(String hql, Map<String, Object> params) {
+        Query query = entityManager.createQuery(hql);
+        if (params != null) {
+            for (String key : params.keySet()) {
+                query.setParameter(key, params.get(key));
+            }
+        }
+        List<T> result = query.getResultList();
+        int total = result.size();
+        return new SearchResult<T>(0, total, total, result);
+    }
+    
 }
--- gumga-domain/src/main/java/gumga/framework/domain/repository/GumgaCrudRepository.java
+++ gumga-domain/src/main/java/gumga/framework/domain/repository/GumgaCrudRepository.java
@@ -6,15 +6,18 @@ import gumga.framework.domain.GumgaRepository;
 import gumga.framework.domain.Pesquisa;
 
 import java.io.Serializable;
+import java.util.Map;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
 public interface GumgaCrudRepository<T, ID extends Serializable> extends JpaRepository<T, ID>, GumgaRepository<T, ID> {
-	
-	SearchResult<T> pesquisa(QueryObject query);
-	
-	Pesquisa<T> pesquisa();
+
+    SearchResult<T> pesquisa(QueryObject query);
+
+    Pesquisa<T> pesquisa();
+
+    public SearchResult<T> hqlSearch(String hql, Map<String, Object> params);
 
 }
