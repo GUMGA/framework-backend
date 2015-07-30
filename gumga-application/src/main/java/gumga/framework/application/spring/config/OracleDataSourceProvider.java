03497ddb3aa8521654c738f91d9aba99c97dea9a
gyowanny <gyowanny@gmail.com>
2015-07-30 14:53:01 -0300

    Homologacao Oracle

---
+++ gumga-application/src/main/java/gumga/framework/application/spring/config/OracleDataSourceProvider.java
@@ -0,0 +1,31 @@
+package gumga.framework.application.spring.config;
+
+import gumga.framework.domain.GumgaQueryParserProvider;
+
+import javax.sql.DataSource;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+public class OracleDataSourceProvider implements DataSourceProvider{
+
+	@Override
+	public DataSource createDataSource(String url, String user, String password) {
+		HikariConfig config = new HikariConfig();
+        GumgaQueryParserProvider.defaultMap = GumgaQueryParserProvider.getOracleLikeMap();
+        config.setDataSourceClassName("oracle.jdbc.pool.OracleDataSource");
+        config.addDataSourceProperty("url", url);
+        config.addDataSourceProperty("user", user);
+        config.addDataSourceProperty("password", password);
+        config.setMaximumPoolSize(20);
+        config.setIdleTimeout(30000L);
+        config.setInitializationFailFast(true);
+        return new HikariDataSource(config);
+	}
+
+	@Override
+	public String getDialect() {
+		return "org.hibernate.dialect.Oracle10gDialect";
+	}
+
+}
