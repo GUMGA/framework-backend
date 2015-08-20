a2239576c62a2709b83000fb72ae622fcae4d93b
munif <munifgebara@gmail.com>
2015-08-20 18:48:35 -0300

    hql gerado com multitipos

--- gumga-core/src/main/java/gumga/framework/core/utils/ReflectionUtils.java
+++ gumga-core/src/main/java/gumga/framework/core/utils/ReflectionUtils.java
@@ -5,40 +5,57 @@ import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
 public class ReflectionUtils {
-	
-	public static Field findField(Class<?> clazz, String name) {
-		return findField(clazz, name, null);
-	}
 
-	public static Field findField(Class<?> clazz, String name, Class<?> type) {
-		if (clazz == null) 
-			throw new IllegalArgumentException("Class must not be null");
-		
-		if (name == null && type == null) 
-			throw new IllegalArgumentException("Either name or type of the field must be specified");
-			
-		Class<?> searchType = clazz;
-		
-		while (!Object.class.equals(searchType) && searchType != null) {
-			Field[] fields = searchType.getDeclaredFields();
-			
-			for (Field field : fields) 
-				if ((name == null || name.equals(field.getName())) && (type == null || type.equals(field.getType()))) 
-					return field;
-			
-			searchType = searchType.getSuperclass();
-		}
-		
-		return null;
-	}
-	
-	public static Class<?> inferGenericType(Class<?> clazz) {
-		return inferGenericType(clazz, 0);
-	}
-	
-	public static Class<?> inferGenericType(Class<?> clazz, int index) {
-		Type superClass = clazz.getGenericSuperclass();
-		return (Class<?>) ((ParameterizedType) superClass).getActualTypeArguments()[index];
-	}
+    public static Field getField(Class clazz, String name) {
+        try {
+            Field toReturn = clazz.getDeclaredField(name);
+            return toReturn;
+        } catch (NoSuchFieldException | SecurityException ex) {
+
+        }
+        if (!Object.class.equals(clazz.getSuperclass())) {
+            return getField(clazz.getSuperclass(), name);
+        }
+        return null;
+    }
+
+    public static Field findField(Class<?> clazz, String name) {
+        return findField(clazz, name, null);
+    }
+
+    public static Field findField(Class<?> clazz, String name, Class<?> type) {
+        if (clazz == null) {
+            throw new IllegalArgumentException("Class must not be null");
+        }
+
+        if (name == null && type == null) {
+            throw new IllegalArgumentException("Either name or type of the field must be specified");
+        }
+
+        Class<?> searchType = clazz;
+
+        while (!Object.class.equals(searchType) && searchType != null) {
+            Field[] fields = searchType.getDeclaredFields();
+
+            for (Field field : fields) {
+                if ((name == null || name.equals(field.getName())) && (type == null || type.equals(field.getType()))) {
+                    return field;
+                }
+            }
+
+            searchType = searchType.getSuperclass();
+        }
+
+        return null;
+    }
+
+    public static Class<?> inferGenericType(Class<?> clazz) {
+        return inferGenericType(clazz, 0);
+    }
+
+    public static Class<?> inferGenericType(Class<?> clazz, int index) {
+        Type superClass = clazz.getGenericSuperclass();
+        return (Class<?>) ((ParameterizedType) superClass).getActualTypeArguments()[index];
+    }
 
 }
