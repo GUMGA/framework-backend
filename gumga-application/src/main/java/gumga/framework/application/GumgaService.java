a57e92448d5d1136cd4ee624291247fa39484c89
munif <munifgebara@gmail.com>
2015-11-05 17:51:56 -0200

    Atualizacoes de testes

--- gumga-application/src/main/java/gumga/framework/application/GumgaService.java
+++ gumga-application/src/main/java/gumga/framework/application/GumgaService.java
@@ -63,7 +63,9 @@ public abstract class GumgaService<T extends GumgaIdable<ID>, ID extends Seriali
     public void delete(T resource) {
         beforeDelete(resource);
         repository.delete(resource);
-        gces.deleteCustomFields(resource);
+        if (gces != null) {
+            gces.deleteCustomFields(resource);
+        }
         afterDelete();
     }
 
@@ -101,7 +103,9 @@ public abstract class GumgaService<T extends GumgaIdable<ID>, ID extends Seriali
 
         beforeSaveOrUpdate(resource, isNew);
         T entity = repository.save(resource);
-        gces.saveCustomFields(resource);
+        if (gces != null) {
+            gces.saveCustomFields(resource);
+        }
         afterSaveOrUpdate(entity, isNew);
 
         return entity;
