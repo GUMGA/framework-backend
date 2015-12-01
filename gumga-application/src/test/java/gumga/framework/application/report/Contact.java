d3b27947919e6441d297724446d79100b8ff2af1
gyowanny <gyowanny@gmail.com>
2015-12-01 09:20:47 -0200

    GUMGA-688

---
+++ gumga-application/src/test/java/gumga/framework/application/report/Contact.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gumga.framework.application.report;
+
+/**
+ *
+ * @author gyowanny
+ */
+public class Contact {
+
+    String name;
+    String phone;
+
+    public Contact(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+}
---
+++ gumga-presentation/src/main/java/gumga/framework/presentation/api/AbstractReportAPI.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gumga.framework.presentation.api;
+
+import gumga.framework.application.service.JasperReportService;
+import gumga.framework.application.service.ReportType;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ *
+ * @author gyowanny
+ */
+public abstract class AbstractReportAPI {
+
+    @Autowired
+    private JasperReportService reportService;
+
+    private String reportsFolder;
+
+    /**
+     * Construtor default
+     *
+     * @param _reportsFolder
+     */
+    public AbstractReportAPI(String _reportsFolder) {
+        reportsFolder = _reportsFolder;
+    }
+
+    public void generateAndExportReport(String reportName, List data, Map<String, Object> params,
+            HttpServletRequest request, HttpServletResponse response, ReportType type) throws Exception {
+        InputStream is = getResourceAsInputStream(request, reportName);
+        setContentType(response, reportName, type);
+        reportService.exportReport(is, response.getOutputStream(), data, params, type);
+    }
+
+    public void generateAndExportHTMLReport(String reportName, String destFile, List data, Map<String, Object> params,
+            HttpServletRequest request, HttpServletResponse response) throws Exception {
+        InputStream is = getResourceAsInputStream(request, reportName);
+        setContentType(response, reportName, ReportType.HTML);
+        reportService.exportReportToHtmlFile(is, data, params, destFile);
+        
+        //Set the output content
+        InputStream generatedIs = request.getServletContext().getResourceAsStream(destFile);
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        int nRead;
+        byte[] bytes = new byte[16384];
+
+        while ((nRead = generatedIs.read(bytes, 0, bytes.length)) != -1) {
+            buffer.write(bytes, 0, nRead);
+        }
+
+        buffer.flush();
+        bytes = buffer.toByteArray();
+        response.setContentLength(bytes.length);
+        response.getOutputStream().write(bytes, 0, bytes.length);
+    }
+
+    protected InputStream getResourceAsInputStream(HttpServletRequest request, String reportName) {
+        return request.getServletContext().getResourceAsStream(getFullPath(reportsFolder, reportName));
+    }
+
+    protected void setContentType(HttpServletResponse response, String reportName, ReportType type) {
+        response.setContentType(type.getContentType());
+        response.setHeader("Content-disposition", "inline; filename=" + reportName);
+    }
+
+    protected String getFullPath(String folder, String file) {
+        String sep = folder.endsWith(File.separator) ? "" : File.separator;
+        return folder.concat(sep).concat(file);
+    }
+
+}
