4cf2d8c9222c33df0c5d1afadfcc545209c352bc
Munif <munifgebara@gmail.com>
2016-02-15 15:20:21 -0200

    Format de datas

---
+++ gumga-domain/src/main/java/gumga/framework/domain/config/GumgaIsoDataSerializer.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gumga.framework.domain.config;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.springframework.stereotype.Component;
+
+/**
+ * Quando o formato não é @Temporal(javax.persistence.TemporalType.DATE) e não
+ * @Temporal(javax.persistence.TemporalType.TIMESTAMP) o framework envia a data
+ * em formato simples, não iso, ocasionando problemas. Assim, foi criado um
+ * formatador para estes casos sem impactar nos sistemas que já tratam as datas.
+ * Para utilizar a formatação, utilizar como no exemplo:
+ *
+ * @JsonSerialize(using = GumgaIsoDataSerializer.class)
+ * @Temporal(javax.persistence.TemporalType.DATE) private Date viewedAt;
+ *
+ *
+ * @author munif
+ */
+@Component
+public class GumgaIsoDataSerializer extends JsonSerializer<Date> {
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+    //private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+
+    @Override
+    public void serialize(Date date, JsonGenerator gen, SerializerProvider provider) throws IOException, JsonProcessingException {
+        String formattedDate = dateFormat.format(date);
+        gen.writeString(formattedDate);
+    }
+
+}
