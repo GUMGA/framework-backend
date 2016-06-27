950c3028314b72cdf9b338592935193919dba84c
Felipe Sabadini Facina <fsabadini@hotmail.com>
2016-06-27 09:13:39 -0300

    converter localdate e localdatetime

---
+++ gumga-domain/src/main/java/gumga/framework/domain/GumgaLocalDateTimeAttributeConverter.java
@@ -0,0 +1,20 @@
+package gumga.framework.domain;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Converter(autoApply = true)
+public class GumgaLocalDateTimeAttributeConverter implements AttributeConverter<LocalDateTime, Timestamp> {
+
+    @Override
+    public Timestamp convertToDatabaseColumn(LocalDateTime localDateTime) {
+        return localDateTime == null ? null : Timestamp.valueOf(localDateTime);
+    }
+
+    @Override
+    public LocalDateTime convertToEntityAttribute(Timestamp timestamp) {
+        return timestamp == null ? null : timestamp.toLocalDateTime();
+    }
+}
