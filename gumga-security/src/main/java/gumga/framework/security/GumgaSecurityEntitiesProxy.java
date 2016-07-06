06d5982c0d9113230cf582a04c9a0cc0102c5e75
Felipe Sabadini Facina <fsabadini@hotmail.com>
2016-07-06 08:34:06 -0300

    correcao freemaker e proxy

--- gumga-security/src/main/java/gumga/framework/security/GumgaSecurityEntitiesProxy.java
+++ gumga-security/src/main/java/gumga/framework/security/GumgaSecurityEntitiesProxy.java
@@ -5,6 +5,8 @@ import com.wordnik.swagger.annotations.ApiOperation;
 import gumga.framework.core.GumgaThreadScope;
 import gumga.framework.core.GumgaValues;
 import java.util.Map;
+
+import org.apache.commons.collections.map.HashedMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,7 +30,14 @@ public class GumgaSecurityEntitiesProxy {
     public Map getAllOrganizations() {
         final String param = "?gumgaToken=" + GumgaThreadScope.gumgaToken.get() + "&pageSize=" + (Integer.MAX_VALUE - 1);
         final String url = gumgaValues.getGumgaSecurityUrl().replace("/publicoperations", "/api/organization") + param;
-        Map  response = restTemplate.getForObject(url, Map.class);
+        Map  response = new HashedMap();
+        try {
+            response = restTemplate.getForObject(url, Map.class);
+            return response;
+        } catch (Exception e) {
+            response.put(403, "Acesso negado!" +  e);
+        }
+
         return response;
     }
 }
